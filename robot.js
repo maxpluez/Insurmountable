04de2e6c1a8b2062abf6bbd0c53a1f06a3a23d7e
Young He <vrownie@ucla.edu>
2022-05-25 18:46:45 -0700

    Restructure robot model to be linear

--- robot.js
+++ robot.js
@@ -24,7 +24,6 @@ export const Robot = class Robot {
         // l_hand->l_wrist->ll_arm
         const l_wrist_location = Mat4.translation(0, -0.3, 0);
         this.l_wrist = new Arc("l_wrist", this.l_hand_node, this.ll_arm_node, l_wrist_location);
-        this.l_hand_node.children_arcs.push(this.l_wrist)
 
         // left upper arm node
         let lu_arm_transform = Mat4.scale(0.8, 0.2, .2);
@@ -33,7 +32,6 @@ export const Robot = class Robot {
         // ll_arm->l_elbow->lu_arm
         const l_elbow_location = Mat4.translation(0, -1.1, 0);
         this.l_elbow = new Arc("l_elbow", this.ll_arm_node, this.lu_arm_node, l_elbow_location);
-        this.ll_arm_node.children_arcs.push(this.l_elbow)
 
         // torso
         const torso_transform = Mat4.scale(1.1, 1.1, 1.1);
@@ -42,7 +40,6 @@ export const Robot = class Robot {
         // lu_arm->l_shoulder->torso
         const l_shoulder_location = Mat4.translation(1.6, 0, 0);
         this.l_shoulder = new Arc("l_shoulder", this.lu_arm_node, this.torso_node, l_shoulder_location);
-        this.lu_arm_node.children_arcs.push(this.l_shoulder)
 
         // right upper arm node
         let ru_arm_transform = Mat4.scale(0.8, 0.2, .2);
@@ -51,7 +48,6 @@ export const Robot = class Robot {
         // torso->r_shoulder->ru_arm
         const r_shoulder_location = Mat4.translation(2.0, 0, 0);
         this.r_shoulder = new Arc("r_shoulder", this.torso_node, this.ru_arm_node, r_shoulder_location);
-        this.torso_node.children_arcs.push(this.r_shoulder)
 
         // right lower arm node
         let rl_arm_transform = Mat4.scale(0.2, 0.6, .2);
@@ -60,7 +56,6 @@ export const Robot = class Robot {
         // ru_arm->r_elbow->rl_arm
         const r_elbow_location = Mat4.translation(1.6, 0, 0);
         this.r_elbow = new Arc("r_elbow", this.ru_arm_node, this.rl_arm_node, r_elbow_location);
-        this.ru_arm_node.children_arcs.push(this.r_elbow)
 
         // right hand node
         let r_hand_transform = Mat4.scale(.3, .3, .3);
@@ -69,7 +64,6 @@ export const Robot = class Robot {
         // rl_arm->r_wrist->r_hand
         const r_wrist_location = Mat4.translation(0, 1.1, 0);
         this.r_wrist = new Arc("r_wrist", this.rl_arm_node, this.r_hand_node, r_wrist_location);
-        this.rl_arm_node.children_arcs.push(this.r_wrist)
     }
 
     // rebase_left_as_root() {} // TODO: switch hands
@@ -83,28 +77,14 @@ export const Robot = class Robot {
     }
 
     draw(webgl_manager, uniforms, transform_matrix, material) {
-        this.matrix_stack = [];
         this._rec_draw(this.root, transform_matrix, webgl_manager, uniforms, material);
     }
 
     _rec_draw(arc, matrix, webgl_manager, uniforms, material) {
         if (arc !== null) {
-            const L = arc.location_matrix;
-            const A = arc.articulation_matrix;
-            matrix.post_multiply(L.times(A));
-            this.matrix_stack.push(matrix.copy());
-
-            const node = arc.child_node;
-            const T = node.transform_matrix;
-            matrix.post_multiply(T);
-            node.shape.draw(webgl_manager, uniforms, matrix, material);
-
-            matrix = this.matrix_stack.pop();
-            for (const next_arc of node.children_arcs) {
-                this.matrix_stack.push(matrix.copy());
-                this._rec_draw(next_arc, matrix, webgl_manager, uniforms, material);
-                matrix = this.matrix_stack.pop();
-            }
+            matrix.post_multiply(arc.location_matrix.times(arc.articulation_matrix));
+            arc.child_node.shape.draw(webgl_manager, uniforms, matrix.times(arc.child_node.transform_matrix), material);
+            this._rec_draw(arc.child_node.child_arc, matrix, webgl_manager, uniforms, material);
         }
     }
 }
@@ -115,7 +95,7 @@ class Node {
         this.shape = shape;
         this.transform_matrix = transform;
         this.parent_arc = null;
-        this.children_arcs = [];
+        this.child_arc = null;
     }
 }
 
@@ -126,7 +106,8 @@ class Arc {
         this.child_node = child;
         this.location_matrix = location;
         this.articulation_matrix = Mat4.identity();
-        child.parent_arc = this;
+        if (parent != null) parent.child_arc = this;
+        if (child != null) child.parent_arc = this;
     }
 
     get_absolute_location() {
